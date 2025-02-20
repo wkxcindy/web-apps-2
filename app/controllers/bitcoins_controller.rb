8c0202b090cfb3b795ea1250048f7ae129c10e34
Cindy Wang <cindy.wang1@kellogg.northwestern.edu>
2025-02-20 00:51:16 +0000

    commit

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -2,7 +2,18 @@ class BitcoinsController < ApplicationController
 
   def index
     # add backend code
-    # render bitcoins/index view
+    bitcoin = 1.0
+    require "net/http"
+    require "json"
+    url = "https://blockchain.info/ticker"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    bitcoin_data = JSON.parse(response)
+    # using the API, extract current rate of bitcoin in USD
+    usd_rate = bitcoin_data["USD"]["last"]
+    # calculate value in USD of user's bitcoin
+  usd_value = usd_rate * bitcoin
+  # render bitcoins/index view
   end
 
 end
